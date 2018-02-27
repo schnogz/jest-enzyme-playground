f6cfdf384dd3575659278b8dac26952a5099eed6
Andrew Schneider <andrew.mark.schneider@gmail.com>
2018-02-26 22:20:45 -0500

    skeleton test app

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+const threshold = 90;
+
+module.exports = {
+	collectCoverageFrom: [
+		'src/**/*.js'
+	],
+	coveragePathIgnorePatterns: [
+		'index.js$',
+		'theme.js$',
+		'redux/enhancers/middlewares.js$',
+		'redux/store.js$'
+	],
+	coverageThreshold: {
+		global: {
+			statements: threshold,
+			branches: threshold,
+			functions: threshold,
+			lines: threshold
+		}
+	},
+	moduleNameMapper: {
+		'^@actions(.*)': '<rootDir>/src/redux/actions$1',
+		'^@assets(.*)': '<rootDir>/src/assets$1',
+		'^@atoms(.*)': '<rootDir>/src/components/atoms$1',
+		'^@molecules(.*)': '<rootDir>/src/components/molecules$1',
+		'^@organisms(.*)': '<rootDir>/src/components/organisms$1',
+		'^@pages(.*)': '<rootDir>/src/components/pages$1',
+		'^@redux(.*)': '<rootDir>/src/redux$1',
+		'^@selectors(.*)': '<rootDir>/src/redux/selectors$1',
+		'^@services(.*)': '<rootDir>/src/services$1',
+		'^@templates(.*)': '<rootDir>/src/components/templates$1'
+	},
+	setupFiles: [
+		'./setup.js'
+	],
+	snapshotSerializers: [
+		'enzyme-to-json/serializer'
+	],
+	testEnvironmentOptions: {
+		url: 'https://abc.def.ghi/base'
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router-dom'
+import { ConnectedRouter } from 'react-router-redux'
+import { ThemeProvider } from 'styled-components'
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+import history from './history'
+import store from './redux/store'
+import theme from './theme'
+
+ReactDOM.render(
+  <AppContainer>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ConnectedRouter history={history}>
+          <Route component={App} />
+        </ConnectedRouter>
+      </ThemeProvider>
+    </Provider>
+  </AppContainer>,
+  document.getElementById('app')
+);
+
+if (module.hot) module.hot.accept();
